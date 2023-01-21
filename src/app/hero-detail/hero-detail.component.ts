8d77265c91f189c5e5bb09d5d7a7fd95455a1c6f
NinhPc <ninhmphpl@gmail.com>
2023-01-21 10:12:59 +0700

    kj

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.scss']
+})
+export class HeroDetailComponent {
+
+}
