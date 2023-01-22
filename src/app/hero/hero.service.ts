750f15d175fb8ad93e567ad606d3f00883b875a6
NinhPc <ninhmphpl@gmail.com>
2023-01-22 22:22:52 +0700

    all

--- src/app/hero/hero.service.ts
+++ src/app/hero/hero.service.ts
@@ -1,15 +1,22 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, retry, throwError } from 'rxjs';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 import { Hero } from './Hero';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'my-auth-token'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  
   constructor(private http: HttpClient) { }  // DI HtppClient for service
+
   public findAll() {
     return this.http.get<Hero[]>("http://localhost:8080/hero").pipe(
       retry(3), // retry times after program has error
@@ -17,6 +24,50 @@ export class HeroService {
     )
   }
 
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    term = term.trim();
+
+    // Add safe, URL encoded search parameter if there is a search term
+    const options = term ? {
+      params: new HttpParams()
+        .set('name', term)
+        .set('number', 123)
+    } : {};
+
+    return this.http.get<Hero[]>("http://localhost:8080/hero", options)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  /** POST: add a new hero to the database */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>("http://localhost:8080/hero", hero, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(id: number): Observable<unknown> {
+    const url = `${"http://localhost:8080/hero"}/${id}`; // DELETE api/heroes/42
+    return this.http.delete(url, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  /** PUT: update the hero on the server. Returns the updated hero upon success. */
+  updateHero(hero: Hero): Observable<Hero> {
+    httpOptions.headers =
+      httpOptions.headers.set('Authorization', 'my-new-auth-token');
+
+    return this.http.put<Hero>("http://localhost:8080/hero", hero, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
 
   // bắt lỗi của chương trình
   private handleError(error: HttpErrorResponse) {
