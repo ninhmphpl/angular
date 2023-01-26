4a198032a24fd7f1d1c029fd31d9917b78f2301d
NinhPc <ninhmphpl@gmail.com>
2023-01-26 20:53:41 +0700

    asdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { NavComponent } from './home/nav/nav.component';
+import { MainComponent } from './home/main/main.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    NavComponent,
+    MainComponent,
+    FooterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
