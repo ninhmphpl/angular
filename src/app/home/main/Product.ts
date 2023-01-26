7b27d2134dc6c14b1194caa474f2bdb1ae94ea6b
NinhPc <ninhmphpl@gmail.com>
2023-01-26 22:46:04 +0700

    ok

--- src/app/home/main/Product.ts
+++ src/app/home/main/Product.ts
@@ -1,9 +1,18 @@
 export interface ProductLite{
     name: string;
     price: number;
-    time: Date;
+    time: number;
 }
 
-// export product : ProductLite[]{
-//     {name: "ninh", }
-// }
+export let product : ProductLite[]= [
+    {name: "ninh", price: 123, time: 0},
+    {name: "game", price: 52345, time: 0},
+    {name: "haha", price: 1221233, time: 0},
+    {name: "iphone", price: 1155523, time: 0},
+    {name: "lap top", price: 12233, time: 0},
+    {name: "may tinh", price: 1523, time: 0},
+    {name: "do choi", price: 12663, time: 0},
+    {name: "bong da", price: 12773, time: 0},
+    {name: "bong bau duc", price: 12553, time: 0},
+]
+    
