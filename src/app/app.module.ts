dd78967bf9eea2b5e525d44d570eb25d97df97b2
NinhPc <ninhmphpl@gmail.com>
2023-01-30 21:35:38 +0700

    full

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadFileComponent } from './upload-file/upload-file.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/upload-file/upload-file.component.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'my-auth-token'
+  })
+};
+
+@Component({
+  selector: 'app-upload-file',
+  templateUrl: './upload-file.component.html',
+  styleUrls: ['./upload-file.component.scss']
+})
+export class UploadFileComponent {
+  public files: File[] = [];
+  public imgSrc: any[] = []
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getFile(file: any) {
+    let f: File = file.target.files.item(0);
+
+    const reader = new FileReader();
+    reader.onload = e => this.imgSrc.push({src: reader.result, file: f});
+    reader.readAsDataURL(f);
+
+    this.files.push(f)
+  }
+
+  public post() {
+    let data = new FormData();
+    for(let i = 0 ; i < this.files.length ; i++ ){
+      data.append("files", this.files[i])
+    }
+    
+    this.http.post('http://localhost:8080', data).subscribe()
+  }
+
+}
