1b74f637ea5ccaca4d0283ae9c409b2ed3313e09
NinhPc <ninhmphpl@gmail.com>
2023-02-02 10:01:36 +0700

    comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/compat/storage';
 import { UploadTaskSnapshot } from '@angular/fire/compat/storage/interfaces';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,28 @@ import { UploadTaskSnapshot } from '@angular/fire/compat/storage/interfaces';
 export class AppComponent {
   constructor(private storage: AngularFireStorage) { }
   public status: any;
+  public path! : string;
+  public perstion! : number;
 
   public upload(event: any) {
     const file: File | null = event.target.files[0]
     if (file) {
       const filePath = file.name;
-      const task = this.storage.upload(filePath, file);
-      // this.status = task.snapshotChanges().subscribe()
+      const fileRef : AngularFireStorageReference = this.storage.ref(filePath);
+
+      fileRef.getDownloadURL().subscribe(url => console.log(url)
+      );
+      
+      const task : AngularFireUploadTask = this.storage.upload(filePath, file);
+      task.snapshotChanges().pipe(
+        finalize(() => {
+          fileRef.getDownloadURL().subscribe(url => {
+            this.path = url
+            console.log(url);            
+          });
+        })
+      ).subscribe()
+      task.percentageChanges().subscribe((per : number | undefined) => {if(per) this.perstion = per})
     }
   }
 }
