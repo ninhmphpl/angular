ebe963a7f07d74415c2682ad9734aa40c93686bd
NinhPc <ninhmphpl@gmail.com>
2023-02-04 16:51:51 +0700

    commit

--- src/app/produt/edit-product/edit-product.component.ts
+++ src/app/produt/edit-product/edit-product.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ErrorHandler } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-product',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-product.component.scss']
 })
 export class EditProductComponent {
+  files : File[] = [];
+  validFile = true;
+  public productForm = this.fb.group({
+    name : ['',[Validators.required, Validators.maxLength(225)]],
+    price : [0, [Validators.min(1)]],
+    description : ['', [Validators.maxLength(3000)]],
+    category : this.fb.group({
+      id : 0
+    })
+  })
+
+  constructor(private fb: FormBuilder) { }
+
+  public submit(){
+
+  }
+
+  getFile(even : any){
+    this.files = even.target.files
+    this.validFile = (this.files.length > 4 || this.files.length == 0)
+  }
+
 
 }
--- src/environments/Product.ts
+++ src/environments/Product.ts
@@ -1,51 +1,53 @@
 export interface ProductLite{
-    id: number;
-    name: string;
-    img: string;
-    price: number;
-    sold: number;
+    id?: number;
+    name?: string;
+    img?: string;
+    price?: number;
+    sold?: number;
 }
 export interface ProductDetail{
-    name : string;
-    rate : number;
-    ratings : number;
-    img : string;
-    imgs : Img[];
-    sold : number;
-    oldPrice : number;
-    newPrice : number;
-    voucher : Voucher[];
-    color : Color[];
-    available : number;
-    category : Category;
-    brand : Brand;
-    shipForm : ShipFrom;
-    description : string;
+    name ?: string;
+    rate ?: number;
+    ratings ?: number;
+    img ?: string;
+    imgs ?: Img[];
+    sold ?: number;
+    oldPrice ?: number;
+    newPrice ?: number;
+    voucher ?: Voucher[];
+    color ?: Color[];
+    available ?: number;
+    category ?: Category;
+    brand ?: Brand;
+    shipForm ?: ShipFrom;
+    description ?: string;
+}
+export class Image implements Img{
 }
 export interface Img{
-    id : number;
-    name: string;
-    url : string;
+    id ?: number;
+    name?: string;
+    url ?: string;
 }
 export interface Voucher{
-    id : number;
-    value: number;
-    min : number;
+    id ?: number;
+    value?: number;
+    min ?: number;
 }
 export interface Color{
-    id : number;
-    name : string;
+    id ?: number;
+    name ?: string;
 }
 export interface Category{
-    id : number;
-    name : string;
+    id ?: number;
+    name ?: string;
 }
 export interface Brand{
-    id : number;
-    url : string;
-    name : string;
+    id ?: number;
+    url ?: string;
+    name ?: string;
 }
 export interface ShipFrom{
-    id : number;
-    name : string;
+    id ?: number;
+    name ?: string;
 }
