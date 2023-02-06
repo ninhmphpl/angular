67d54974c48b0a7810a6de939b0594ff44aca36b
NinhPc <ninhmphpl@gmail.com>
2023-02-06 20:28:18 +0700

    end Module 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { environtment } from 'src/environtments/environment';
 import { Tour } from './model/model';
-
+import { TourService } from './service/tour.service';
+const url = environtment.url
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  tours : Tour[] = []
+export class AppComponent implements OnInit {
+  action = 0;
+  tours : Tour[] = [];
+  tourForm = this.fb.group<Tour>(new Tour());
+  tourDetail! : Tour;
 
-  
+  constructor(
+    private service : TourService,
+    private fb : FormBuilder
+  ){}
+
+
+  ngOnInit(): void {
+    this.getList()
+  }
+  getList(){
+    this.action = 0
+    this.service.getArray(url).subscribe(data => this.tours = data)
+  }
+
+  formUpdate(event : any){
+    this.action = 1
+    this.service.getOne(`${url}/${event}`).subscribe(
+      data => this.tourForm.patchValue(data)
+    )
+  }
+
+  formCreate(){
+    this.action = 1
+    this.tourForm = this.fb.group<Tour>(new Tour());
+  }
+
+  deleteTour(event : any){
+    this.service.delete(`${url}/${event}`).subscribe(()=>this.getList())
+    
+  }
+
+  detail(event : any){
+    this.action = 2
+    for(let i of this.tours){
+      if(i.id === event) this.tourDetail = i
+    }
+  }
+
+  submit(){
+    let data = this.tourForm.value
+    if(this.action == 1 && data.id != 0){
+      this.service.put(`${url}/${data.id}`, data).subscribe(()=>{this.getList()})
+    }else if(this.action == 1){
+      this.service.post(`${url}`, data).subscribe(()=>{this.getList()})
+    }
+    
+  }
 }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,9 +1,70 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, retry, throwError } from 'rxjs';
+import { HttpOptions } from '../../environtments/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
+export class ApiService<E> {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  public getArray(url : string, httpOptions? : HttpOptions) : Observable<E[]> {
+    return this.http.get<E[]>(url, httpOptions).pipe(
+      retry(3), // retry times after program has error
+      catchError(this.handleError)
+    )
+  }
+
+  getOne(url : string, httpOptions? : HttpOptions): Observable<E> {
+    return this.http.get<E>(url, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  /** POST: add a new object to the database */
+  post(url : string , object : any, httpOptions? : HttpOptions): Observable<E> {
+    return this.http.post<E>(url, object, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  /** DELETE: delete the hero from the server */
+   delete(url : string, httpOptions? : HttpOptions): Observable<unknown> {
+    return this.http.delete(url , httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  /** PUT: update the object on the server. Returns the updated hero upon success. */
+  put( url : string, object: any, httpOptions? : HttpOptions): Observable<E> {
+    return this.http.put<E>(url , object , httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+
+
+    // bắt lỗi của chương trình
+    private handleError(error: HttpErrorResponse) {
+      if (error.status === 0) {
+        // Lỗi trả về từ client
+        // A client-side or network error occurred. Handle it accordingly.
+        console.error('An error occurred:', error.error);
+      } else {
+        // Lỗi trả về từ máy chủ
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong.
+        console.error(
+          `Backend returned code (Xảy ra lỗi phía máy chủ) ${error.status}, body was (Dữ liệu trả về): `, error.error);
+      }
+      // Lỗi không rõ
+      // Return an observable with a user-facing error message.
+      return throwError(() => new Error('Something bad happened; please try again later.(Lỗi không rõ)'));
+    }
 }
