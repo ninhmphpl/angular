bfc0e173617c217480b30970e7fecc7be03ea985
ninhmphpl <ninhmphpl@gmail.com>
2023-03-11 14:51:03 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayGameComponent } from './play-game/play-game.component';
+import { TableComponent } from './play-game/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayGameComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/play-game/play-game.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-play-game',
+  templateUrl: './play-game.component.html',
+  styleUrls: ['./play-game.component.css']
+})
+export class PlayGameComponent {
+
+}
