05a3fd16ad5306c31963937703b4fa99af3db370
ninhmphpl <ninhmphpl@gmail.com>
2023-09-11 07:02:12 +0700

    cmt

---
+++ src/app/music.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "./Environment";
+
+const urlPatrol = environment.hostPatrol
+@Injectable({
+  providedIn: 'root'
+})
+export class MusicService {
+  musics : any;
+  constructor(private http : HttpClient) { }
+  get() {
+    this.http.get(urlPatrol + "/music?edit=true").subscribe((payload: any) => {
+      if (payload.code == 200) {
+        this.musics = payload.data
+      } else {
+        alert(payload.data)
+      }
+    }, (error: any) => {
+      alert(JSON.stringify(error.error.detail))
+    })
+  }
+}
