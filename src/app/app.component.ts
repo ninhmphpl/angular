31680865e47dead75f7a7eff70f272bb9544b671
ninhmphpl <ninhmphpl@gmail.com>
2023-09-11 17:42:43 +0700

    cmt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Home} from "./model";
+import {Home, Painting} from "./model";
 
 @Component({
   selector: 'app-root',
@@ -17,6 +17,15 @@ export class AppComponent implements OnInit{
       alert(JSON.stringify(error))
     })
   }
+  addBanner(){
+    this.home.banner.push(new Painting("BANNER"))
+  }
+  addFeature(){
+    this.home.features.push(new Painting("FEATURES"))
+  }
+  addAiRemover(){
+    this.home.aiRemover.push(new Painting("AI_REMOVER"))
+  }
   ngOnInit(): void {
     this.get()
   }
@@ -27,12 +36,73 @@ export class AppComponent implements OnInit{
       alert(JSON.stringify(error))
     })
   }
-  save(model : any){
-    this.http.post(this.domain + "/pain/painting-style", model).subscribe((payload : any)=>{
-      this.get()
+  save(){
+    this.http.post(this.domain + "/pain/home", this.home).subscribe((payload : any)=>{
+      this.home = payload.data
+      alert("OK")
     }, (error : any)=>{
       alert(JSON.stringify(error))
     })
+  }
+  setCoverImg(file : any, pain : any){
+    this.uploadFile(file[0], this.domain.replace("http://", "ws://") + "/upload", "", (urlDownload : any)=>{
+      pain.coverImgUrl = urlDownload
+    })
+  }
+  setAfterImg(file : any, pain : any){
+    this.uploadFile(file[0], this.domain.replace("http://", "ws://") + "/upload", "", (urlDownload : any)=>{
+      pain.afterImgUrl = urlDownload
+    })
+  }
+  setBeforeImg(file : any, pain : any){
+    this.uploadFile(file[0], this.domain.replace("http://", "ws://") + "/upload", "", (urlDownload : any)=>{
+      pain.beforeImgUrl = urlDownload
+    })
+  }
+
+  // afterImgUrl: any
+  // beforeImgUrl: any
+
+  uploadFile(file: File, url: string, path: string, action: (url: string) => any, percentAction?: (data: number) => any) {
+    const chunkSize = 1024 * 7; // Size of each piece of data (1 MB)
+    const totalChunks = Math.ceil(file.size / chunkSize); // data total
+    let fileType = file.name.split('.').pop()
+    let currentChunk = 0; // number percent downloaded
+    let sockets = new WebSocket(url + `?path=${path}&type=${fileType}&size=${file.size}`);
+    sockets.onopen = () => {
+      console.log("connected")
+      const reader = new FileReader();
+      reader.onload = (event: any) => {
+        const data: ArrayBuffer = event.target.result;
+        sockets.send(data);
+        currentChunk++;
+        if (currentChunk < totalChunks) {
+          // If you haven't uploaded all the data yet, continue to upload the next part
+          const start = currentChunk * chunkSize;
+          const end = Math.min(start + chunkSize, file.size);
+          const nextChunk = file.slice(start, end);
+          reader.readAsArrayBuffer(nextChunk);
+        }
+      };
+      // Start uploading the first piece of data
+      const firstChunk = file.slice(0, chunkSize);
+      reader.readAsArrayBuffer(firstChunk);
+    }
+    // finish upload, return url file
+    sockets.onmessage = function (event) {
+      let load: string = event.data;
+      let arr: string[] = load.split("|")
+      switch (arr[0]) {
+        case "path" :
+          action(arr[1]);
+          break;
+        case "error":
+          console.log(arr[1])
+          break;
+        case "size":
+          if (percentAction) percentAction(parseFloat(arr[1]))
+      }
+    };
   }
 
 }
--- src/app/model.ts
+++ src/app/model.ts
@@ -1,20 +1,43 @@
-export interface Home{
+export class Home {
+
+  constructor() {
+    this.banner = [];
+    this.artis = new Painting("NONE");
+    this.magicPainting = new Painting("NONE");
+    this.features = [];
+    this.aiRemover = [];
+  }
+
   banner: Painting[]
-  artis : Painting
+  artis: Painting
   magicPainting: Painting
   features: Painting[]
   aiRemover: Painting[]
 
 }
 
-export interface Painting{
-  id : any
-  name : any
-  type : any
-  description : any
-  coverImgUrl : any
-  afterImgUrl : any
-  beforeImgUrl : any
-  styles : {name:string, description : string}[]
-  keyWord : string[]
+export class Painting {
+  constructor(type : string) {
+    this.styles = [];
+    this.keyWord = [];
+    this.type = type
+  }
+  id: any
+  name: any
+  type: any
+  description: any
+  coverImgUrl: any
+  afterImgUrl: any
+  beforeImgUrl: any
+  styles: Style[]
+  keyWord: string[]
+}
+
+export class Style {
+  constructor() {
+    this.name = "new Name";
+    this.description = "new Description";
+  }
+  name: string
+  description: string
 }
