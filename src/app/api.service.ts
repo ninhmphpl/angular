07e5b48564e16652d3172f29a57463401bd27e26
ninhmphpl <ninhmphpl@gmail.com>
2023-10-04 11:27:08 +0700

    update top

---
+++ src/app/api.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {Auth, getAuth, GoogleAuthProvider, signInWithPopup} from "@angular/fire/auth";
+import {Router} from "@angular/router";
+import {environment} from "./Environment";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+}
