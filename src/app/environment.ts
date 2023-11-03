28e9edc789e7da06f9920bbed08c45750f440e67
ninh <ninhmphpl@gmail.com>
2023-11-03 10:34:16 +0700

    Update

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // url : "http://localhost:3000",
-  url : "https://avatarapi.nowtechai.com",
+  url : "http://localhost:3000",
+  // url : "https://avatarapi.nowtechai.com",
   keySaveToken : "TOKEN_AVATAR"
 }
