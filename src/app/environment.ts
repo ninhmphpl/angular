0c63a877e624ab1562ed26e4ad7d2c6429957a8c
ninh <ninhmphpl@gmail.com>
2023-11-07 13:45:53 +0700

    Update

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  url : "http://localhost:9000",
-  // url : "http://themeios.tolimanspaceni.com",
+  // url : "http://localhost:9000",
+  url : "http://themeios.tolimanspaceni.com",
   keySaveToken : "TOKEN-WALL-PAPER"
 }
