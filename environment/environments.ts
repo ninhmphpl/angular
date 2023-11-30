eb264fa1eba06848d662d13fa390860811420ba4
ninh <ninhmphpl@gmail.com>
2023-11-30 17:47:12 +0700

    Update first in tap tap

--- environment/environments.ts
+++ environment/environments.ts
@@ -1,8 +1,7 @@
 import Swal from 'sweetalert2';
 
 export const environment = {
-  url : "http://45.79.16.25:8082",
-  // url : "http://localhost:8082",
+  url : "http://theme.tolimanspaceni.com",
   keySaveToken : "Paw-Patrol-X-Token",
   firebase: {
     apiKey: "AIzaSyBk7gs8EjXEiVROAY326uHKdFC6eLLNE8A",
