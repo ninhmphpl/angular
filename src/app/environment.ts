c17a55fdfa3394d5b89795e7a993f584fdf85e92
ninh <ninhmphpl@gmail.com>
2024-01-12 13:52:07 +0700

    update 04/01/2024

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  url : "http://localhost:8080",
-  // url : "http://couple.nowtechai.com",
+  // url : "http://localhost:8080",
+  url : "http://couple.nowtechai.com",
   keySaveToken : "TOKEN-PROX"
 }
