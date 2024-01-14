23a5de58bcd9172b9bab4040b94130f179772c4c
ninh <ninhmphpl@gmail.com>
2024-01-14 15:16:12 +0700

    update 12/01/2024 : update r2 (cloudflare) upload

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   // url : "http://localhost",
-  url : "http://prediction.procyoni.com",
+  // url : "http://prediction.procyoni.com",
+  url : "http://palestine-test.heratech.space",
   keySaveToken : "TOKEN-AI-ART"
 }
