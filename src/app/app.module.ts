a8e719e1c5d188ea27ae52c31da328205a0f2e59
ninh <ninhmphpl@gmail.com>
2024-01-14 17:33:57 +0700

    update 12/01/2024 : update r2 (cloudflare) upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ImgUploadComponent} from "./img-upload/img-upload.component";
 import {LoginComponent} from "./login/login.component";
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
@@ -12,11 +11,12 @@ import { ThemeComponent } from './theme/theme.component';
 import { AiPosterComponent } from './ai-poster/ai-poster.component';
 import { AiProfileComponent } from './ai-profile/ai-profile.component';
 import { TemplateComponent } from './template/template.component';
+import {UploadCloudFlareComponent} from "./upload-cloud-flare/upload-cloud-flare.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImgUploadComponent,
+    UploadCloudFlareComponent,
     LoginComponent,
     CategoryComponent,
     ThemeComponent,
--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   // url : "http://localhost:9000",
-  url : "http://themeios.tolimanspaceni.com",
+  // url : "http://themeios.tolimanspaceni.com",
+  url : "http://theme-ios-test.heratech.space",
   keySaveToken : "TOKEN-WALL-PAPER"
 }
