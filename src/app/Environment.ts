d64ede72b0a425c89ca93038350920c2b9555332
ninh <ninhmphpl@gmail.com>
2024-01-15 17:31:22 +0700

    update 12/01/2024 : update r2 (cloudflare) upload

--- src/app/Environment.ts
+++ src/app/Environment.ts
@@ -1,7 +1,7 @@
 import {HttpHeaders} from "@angular/common/http";
 
 export const environment = {
-  url : "http://74.50.81.148", // Expand
+  url : "http://expand-test.heratech.space", // Expand
   keySaveToken : "Paw-Patrol-X-Token",
   firebase: {
     apiKey: "AIzaSyBk7gs8EjXEiVROAY326uHKdFC6eLLNE8A",
