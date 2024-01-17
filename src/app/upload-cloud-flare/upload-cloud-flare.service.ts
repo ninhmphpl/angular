560f0f2db02a6bfc99fa5fba02a7eefb460715d1
ninh <ninhmphpl@gmail.com>
2024-01-17 15:52:47 +0700

    update 12/01/2024 : update r2 (cloudflare) upload

--- src/app/upload-cloud-flare/upload-cloud-flare.service.ts
+++ src/app/upload-cloud-flare/upload-cloud-flare.service.ts
@@ -3,9 +3,9 @@ import {HttpClient} from "@angular/common/http";
 
 const uploadUrl = 'http://s3.heratech.space/cloudflare/upload-file';
 const params = {
-  folder: 'prediction/assets',
-  bucket: 'prox-bucket',
-  domain: 'http://proxglobal.co/'
+  folder: '',
+  bucket: 'expand',
+  domain: 'http://expand.proxglobal.co/'
 };
 @Injectable({
   providedIn: 'root'
