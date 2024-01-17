d75ab6796b2755621496a7c68f8668b33b7bd164
ninh <ninhmphpl@gmail.com>
2024-01-17 18:03:54 +0700

    update 17/01/2024 : update cho cartoon

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  // url : "http://localhost",
-  // url : "http://prediction.procyoni.com",
-  url : "http://accurate.procyoni.com",
+  url : "http://cartoon.procyoni.com",
   keySaveToken : "TOKEN-AI-ART"
 }
