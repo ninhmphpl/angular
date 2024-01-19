0305a62bbdc5b803e7dd7a630aba0dec4dc18af3
ninh <ninhmphpl@gmail.com>
2024-01-19 10:05:39 +0700

    update 17/01/2024 : update cho cartoon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ImgUploadComponent} from "./img-upload/img-upload.component";
 import {LoginComponent} from "./login/login.component";
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { TrendingComponent } from './trending/trending.component';
-import { TredingTypeComponent } from './treding-type/treding-type.component';
-import { MusicComponent } from './music/music.component';
-import { StyleComponent } from './style/style.component';
-import { StyleTextComponent } from './style-text/style-text.component';
-import { StyleImageComponent } from './style-image/style-image.component';
-import { StickerComponent } from './sticker/sticker.component';
 import { UploadMultipleComponent } from './upload-multiple/upload-multiple.component';
 import { UploadCloudFlareComponent } from './upload-cloud-flare/upload-cloud-flare.component';
-import { PopupTrendingComponent } from './popup-trending/popup-trending.component';
+import { AlphabetRenderComponent } from './alphabet-render/alphabet-render.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImgUploadComponent,
     LoginComponent,
-    TrendingComponent,
-    TredingTypeComponent,
-    MusicComponent,
-    StyleComponent,
-    StyleTextComponent,
-    StyleImageComponent,
-    StickerComponent,
     UploadMultipleComponent,
     UploadCloudFlareComponent,
-    PopupTrendingComponent,
+    AlphabetRenderComponent,
     // PopupTrendingComponent
   ],
   imports: [
