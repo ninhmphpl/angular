669b3e1208198ae5eb643702f44e7acdb1502817
ninh <ninhmphpl@gmail.com>
2024-01-19 10:05:45 +0700

    update 17/01/2024 : update cho cartoon

---
+++ src/app/alphabet-render/alphabet-render.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alphabet-render',
+  templateUrl: './alphabet-render.component.html',
+  styleUrls: ['./alphabet-render.component.scss']
+})
+export class AlphabetRenderComponent {
+
+}
