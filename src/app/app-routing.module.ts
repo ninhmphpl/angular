dbb59d889ad4c0d4a197fa52f5dfb287f36f03dd
ninh <ninhmphpl@gmail.com>
2024-01-22 10:17:51 +0700

    22/01/2024

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {TemplateComponent} from "./template/template.component";
+import {AlphabetRenderComponent} from "./alphabet-render/alphabet-render.component";
 
 const routes: Routes = [
+  {path:"template",component:TemplateComponent},
+  {path:"alphabet",component:AlphabetRenderComponent}
 ];
 
 
