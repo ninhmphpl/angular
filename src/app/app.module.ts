375b6680522b7980a169cdb3415e1864ff77e987
ninh <ninhmphpl@gmail.com>
2024-04-15 11:38:43 +0700

    06/02/2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModelSettingComponent } from './setting/model-setting/model-setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelSettingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/setting/model-setting/model-setting.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-setting',
+  templateUrl: './model-setting.component.html',
+  styleUrls: ['./model-setting.component.scss']
+})
+export class ModelSettingComponent {
+  model : any;
+
+
+
+}
