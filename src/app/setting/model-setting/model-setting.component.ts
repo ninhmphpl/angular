6089d98fe9159da2a8e6718343d5dee50f5b2906
ninh <ninhmphpl@gmail.com>
2024-04-15 16:12:40 +0700

    06/02/2024

--- src/app/setting/model-setting/model-setting.component.ts
+++ src/app/setting/model-setting/model-setting.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environment";
 
 @Component({
   selector: 'app-model-setting',
   templateUrl: './model-setting.component.html',
   styleUrls: ['./model-setting.component.scss']
 })
-export class ModelSettingComponent {
-  model : any;
-
-
-
+export class ModelSettingComponent implements  OnInit{
+  model: any;
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(): void {
+    this.getSetting();
+  }
+  getSetting() {
+    this.http.get(environment.apiUrl + "/api/v1/model").subscribe((data) => {
+      this.model = data;
+      console.table(this.model.entries);
+    });
+  }
+  updateSetting() {
+    this.http.post(environment.apiUrl + "/api/v1/model", this.model).subscribe((data) => {
+      this.model = data;
+      console.table(this.model.entries);
+    });
+  }
 }
